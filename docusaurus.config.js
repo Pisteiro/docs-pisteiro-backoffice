a57f1e7d364d15bea9f3a6b9525e553302817343
Camiloprjr <83073480+Camiloprjr@users.noreply.github.com>
2021-10-21 16:28:15 -0300

    doc

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Pisteiro',
   tagline: 'Pisteiro',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://pisteiro.github.io/docs-pisteiro-backoffice',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
